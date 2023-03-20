95cce72e7754b2debbfed998ce990940a9eaa831
Leone Costa Rocha <leocr_lem@yahoo.com.br>
2023-03-20 12:20:12 -0300

    ajuste 01

--- SmartDigitalPsico/SmartDigitalPsico.UI.DashBoard/src/app/app.routing.ts
+++ SmartDigitalPsico/SmartDigitalPsico.UI.DashBoard/src/app/app.routing.ts
@@ -2,17 +2,24 @@ import { Routes } from '@angular/router';
 import { AdminLayoutComponent } from './layouts/admin/admin-layout.component';
 import { AuthLayoutComponent } from './layouts/auth/auth-layout.component';
 
-export const AppRoutes: Routes = [/*{
+export const AppRoutes: Routes = [
+    /*{
         path: '',
         redirectTo: 'dashboard',
         pathMatch: 'full',
-    },*/{
+    },*/
+    {
+        path: 'dashboard',
+        component: AdminLayoutComponent,
+        children: [{
+            path: '',
+            loadChildren: () => import('./dashboard/dashboard.module').then(x => x.DashboardModule)
+        }]
+    },
+    {
         path: '',
         component: AdminLayoutComponent,
         children: [{
-            path: 'dashboard',
-            loadChildren: () => import('./dashboard/dashboard.module').then(x => x.DashboardModule)
-        }, {
             path: '',
             loadChildren: () => import('./userpage/user.module').then(x => x.UserModule)
         }, {
